00897efb16b770afa2b327c47adfc073fd83acd1
MaferMendoza <coloradomendozafernanda@gmail.com>
2023-09-25 17:12:25 -0600

    cambios en el archivo de contact

--- contact.php
+++ contact.php
@@ -48,148 +48,92 @@
 
     <body>
 
-        <main>
+    <main>
 
-            <header class="site-header">
-                <div class="section-overlay"></div>
+<header class="site-header">
+    <div class="section-overlay"></div>
 
-                <div class="container">
+    <div class="container">
+        <div class="row">
+            
+            <div class="col-lg-12 col-12 text-center">
+                <h1 class="text-white">Ponte en contacto</h1>
+
+             
+            </div>
+
+        </div>
+    </div>
+</header>
+
+<section class="contact-section section-padding">
+    <div class="container">
+        <div class="row justify-content-center">
+
+           
+
+         
+        
+            <div class="col-lg-8 col-12 mx-auto">
+                <form class="custom-form contact-form" action="funciones/confirmacion.php" method="post" role="form">
+                    <h2 class="text-center mb-4">¿Tienes un proyecto en mente o duda? Ponte en contacto con nosotros</h2>
+
                     <div class="row">
-                        
-                        <div class="col-lg-12 col-12 text-center">
-                            <h1 class="text-white">Ponte en contacto</h1>
+                        <div class="col-lg-6 col-md-6 col-12">
+                            <label for="first-name">Nombre completo</label>
 
-                            <nav aria-label="breadcrumb">
-                                <ol class="breadcrumb justify-content-center">
-                                    <li class="breadcrumb-item"><a href="index.html">Principal</a></li>
-
-                                    <li class="breadcrumb-item active" aria-current="page">Contacto</li>
-                                </ol>
-                            </nav>
+                            <input type="text" name="full-name" id="full-name" class="form-control" placeholder="Escribe tu nombre" required>
                         </div>
 
+                        <div class="col-lg-6 col-md-6 col-12">
+                            <label for="email">Email</label>
+
+                             <input type="email" name="email" id="email" pattern="[^ @]*@[^ @]*" class="form-control" placeholder="Escribe tu correo electronico" required>
+                        </div>
+
+                        <div class="col-lg-12 col-12">
+                            <label for="message">Mensaje</label>
+
+                            <textarea name="message" rows="6" class="form-control" id="message" placeholder="¿En que puedo ayudarte?"></textarea>
+                        </div>
+
+                        <div class="col-lg-4 col-md-4 col-6 mx-auto">
+                            <button type="submit" class="form-control">Enviar mensaje</button>
+                            <a href="#">
+                        </div>
                     </div>
+                </form>
+            </div>
+
+        </div>
+    </div>
+</section>
+<br><br><br>
+
+<section class="cta-section">
+    <div class="section-overlay"></div>
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-6 col-10">
+                <h2 class="text-white mb-2">Donde las Ideas se Hacen Realidad</h2>
+
+                <p class="text-white">¡Únete a nuestra comunidad de apasionados por la innovación! En "Proyectos Brillantes", estamos comprometidos en impulsar el potencial de mentes creativas y emprendedoras. Ayúdanos a convertir ideas en realidad.</p>
+            </div>
+
+            <div class="col-lg-4 col-12 ms-auto">
+                <div class="custom-border-btn-wrap d-flex align-items-center mt-lg-4 mt-2">
+                    <a href="#" class="custom-btn custom-border-btn btn me-4">Crea una cuenta</a>
+
+                    <a href="#" class="custom-link">Contacto</a>
                 </div>
-            </header>
+            </div>
 
-
-            <section class="contact-section section-padding">
-                <div class="container">
-                    <div class="row justify-content-center">
-
-                        <div class="col-lg-6 col-12 mb-lg-5 mb-3">
-                            <iframe class="google-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.4660414061545!2d-99.18257002591089!3d19.435463540591638!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1f8ad94aba5ab%3A0xd416092c18a9ec2c!2sLeibnitz%20270%2C%20Anzures%2C%20Miguel%20Hidalgo%2C%2011590%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!5e0!3m2!1ses-419!2smx!4v1694551715611!5m2!1ses-419!2smx" width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                            
-                        </div>
-
-                        <div class="col-lg-5 col-12 mb-3 mx-auto">
-                            <div class="contact-info-wrap">
-                                <div class="contact-info d-flex align-items-center mb-3">
-                                    <i class="custom-icon bi-building"></i>
-
-                                    <p class="mb-0">
-                                        <span class="contact-info-small-title">Oficina</span>
-
-                                        Leibnitz #270 Colonia Anzures 
-                                    </p>
-                                </div>
-
-                                <div class="contact-info d-flex align-items-center">
-                                    <i class="custom-icon bi-globe"></i>
-
-                                    <p class="mb-0">
-                                        <span class="contact-info-small-title">Website</span>
-
-                                        <a href="#" class="site-footer-link">
-                                            www.Proyectosbrillantes.com
-                                        </a>
-                                    </p>
-                                </div>
-
-                                <div class="contact-info d-flex align-items-center">
-                                    <i class="custom-icon bi-telephone"></i>
-
-                                    <p class="mb-0">
-                                        <span class="contact-info-small-title">Teléfono</span>
-
-                                        <a href="tel: 305-240-9671" class="site-footer-link">
-                                            305-240-9671
-                                        </a>
-                                    </p>
-                                </div>
-
-                                <div class="contact-info d-flex align-items-center">
-                                    <i class="custom-icon bi-envelope"></i>
-
-                                    <p class="mb-0">
-                                        <span class="contact-info-small-title">Email</span>
-
-                                        <a href="mailto:info@yourgmail.com" class="site-footer-link">
-                                            info@proyectosbrillantes.com
-                                        </a>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-8 col-12 mx-auto">
-                            <form class="custom-form contact-form" action="#" method="post" role="form">
-                                <h2 class="text-center mb-4">Tienes un proyecto en mente? Hablemos</h2>
-
-                                <div class="row">
-                                    <div class="col-lg-6 col-md-6 col-12">
-                                        <label for="first-name">Nombre completo</label>
-
-                                        <input type="text" name="full-name" id="full-name" class="form-control" placeholder="Manuel Pérez" required>
-                                    </div>
-
-                                    <div class="col-lg-6 col-md-6 col-12">
-                                        <label for="email">Email</label>
-
-                                         <input type="email" name="email" id="email" pattern="[^ @]*@[^ @]*" class="form-control" placeholder="Jackdoe@gmail.com" required>
-                                    </div>
-
-                                    <div class="col-lg-12 col-12">
-                                        <label for="message">Mensaje</label>
-
-                                        <textarea name="message" rows="6" class="form-control" id="message" placeholder="¿En que puedo ayudarte?"></textarea>
-                                    </div>
-
-                                    <div class="col-lg-4 col-md-4 col-6 mx-auto">
-                                        <button type="submit" class="form-control">Enviar mensaje</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-
-                    </div>
-                </div>
-            </section>
-
-            <section class="cta-section">
-                <div class="section-overlay"></div>
-            
-                <div class="container">
-                    <div class="row">
-            
-                        <div class="col-lg-6 col-10">
-                            <h2 class="text-white mb-2">Donde las Ideas se Hacen Realidad</h2>
-            
-                            <p class="text-white">¡Únete a nuestra comunidad de apasionados por la innovación! En "Proyectos Brillantes", estamos comprometidos en impulsar el potencial de mentes creativas y emprendedoras. Ayúdanos a convertir ideas en realidad.</p>
-                        </div>
-            
-                        <div class="col-lg-4 col-12 ms-auto">
-                            <div class="custom-border-btn-wrap d-flex align-items-center mt-lg-4 mt-2">
-                                <a href="#" class="custom-btn custom-border-btn btn me-4">Crea una cuenta</a>
-            
-                                <a href="#" class="custom-link">Contacto</a>
-                            </div>
-                        </div>
-            
-                    </div>
-                </div>
-            </section>
-            </main>
+        </div>
+    </div>
+</section>
+</main>
             
               <!-- footer -->
     <?php
