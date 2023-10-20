1c7a5d9c75e060e8bef14401346c0334b2a5748e
Alberto <cuacho.ax@gmail.com>
2023-10-20 13:51:33 -0600

    Categorias de inversionista

---
+++ SocialySinFines_inversionista.php
@@ -0,0 +1,388 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>Social y sin fines</title>
+
+        <!-- CSS FILES -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+        <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100;300;400;600;700&display=swap" rel="stylesheet">
+
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <link href="css/bootstrap-icons.css" rel="stylesheet">
+
+        <link href="css/owl.carousel.min.css" rel="stylesheet">
+
+        <link href="css/owl.theme.default.min.css" rel="stylesheet">
+
+        <link href="css/tooplate-gotto-job.css" rel="stylesheet">
+        
+
+
+    </head>
+
+    <?php
+
+    // Comprueba si el usuario ha iniciado sesión
+    if (isset($_SESSION['Correo'])) {
+        // Header para usuarios que han iniciado sesión
+        include 'header-usuario.php';
+    } else {
+        // Header para usuarios que aún no han iniciado sesión
+        include 'header.php';
+    }
+    ?>
+    
+    <body class="Creador-y-desarrollador-page" id="top">
+
+        <main>
+
+            <header class="site-header">
+                <div class="section-overlay"></div>
+
+                <div class="container">
+                    <div class="row">
+                        
+                        <div class="col-lg-12 col-12 text-center">
+                            <h1 class="text-white">Proyectos sociales y sin fines de lucro.</h1>
+
+                        </div>
+
+                    </div>
+                </div>
+            </header>
+
+            <section class="section-padding pb-0 d-flex justify-content-center align-items-center">
+                <div class="container">
+                    <div class="row">
+
+                        <div class="col-lg-12 col-12">
+                            <form class="custom-form hero-form" action="#" method="get" role="form">
+                                <h3 class="text-white mb-3">Elije entre nuestros tipos de proyectos</h3>
+                                
+                                <div class="row">
+                                    <div class="col-lg-6 col-md-6 col-12">
+                                        <div class="input-group">
+                                            <span class="input-group-text" id="basic-addon1"><i class="bi-laptop custom-icon"></i></span>
+
+                                            <select class="form-select form-control" name="job-salary" id="job-salary" aria-label="Default select example">
+                                                <option selected>Tipo de proyecto.</option>
+                                                <option value="1">Aplicaciones educativas interactivas.</option>
+                                                <option value="2">Herramientas de automatización empresarial.</option>
+                                                <option value="3">Aplicaciones de sostenibilidad ambiental.</option>
+                                                <option value="4">Tecnologías financieras innovadoras.</option>
+                                                <option value="5">Videojuegos.</option> 
+                                            </select>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-lg-12 col-12">
+                                        <button type="submit" class="form-control">
+                                            Buscar proyectos
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+
+                        <div class="col-lg-6 col-12">
+                            <img src="images/4557388.png" class="hero-image img-fluid" alt="">
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
+
+            <section class="job-section section-padding">
+                <div class="container">
+                    <div class="row align-items-center">
+
+                        <div class="col-lg-6 col-12 mb-lg-4">
+                            <h3>Tecnologías financieras innovadoras..</h3>
+                        </div>
+
+                        <div class="col-lg-4 col-12 d-flex align-items-center ms-auto mb-5 mb-lg-4">
+                            <p class="mb-0 ms-lg-auto">Sort by:</p>
+
+                            <div class="dropdown dropdown-sorting ms-3 me-4">
+                                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownSortingButton" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Newest Jobs
+                                </button>
+
+                                <ul class="dropdown-menu" aria-labelledby="dropdownSortingButton">
+                                    <li><a class="dropdown-item" href="#">Lastest Jobs</a></li>
+
+                                    <li><a class="dropdown-item" href="#">Highed Salary Jobs</a></li>
+
+                                    <li><a class="dropdown-item" href="#">Internship Jobs</a></li>
+                                </ul>
+                            </div>
+
+                            <div class="d-flex">
+                                <a href="#" class="sorting-icon active bi-list me-2"></a>
+
+                                <a href="#" class="sorting-icon bi-grid"></a>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6 col-12">
+                            <div class="job-thumb job-thumb-box">
+                                <div class="job-image-box-wrap">
+                                    <a href="job-details.html">
+                                        <img src="images/jobs/ahorro-automatico.jpg" class="job-image img-fluid" alt="">
+                                    </a>
+
+                                    <div class="job-image-box-wrap-info d-flex align-items-center">
+                                    </div>
+                                </div>
+
+                                <div class="job-body">
+                                    <h4 class="job-title">
+                                        <a href="job-details.html" class="job-title-link">Aplicacion de ahorro automatico.</a>
+                                    </h4>
+
+                                    <div class="d-flex align-items-center">
+                                        <div class="job-image-wrap d-flex align-items-center bg-white shadow-lg mt-2 mb-4">
+                                        </div>
+
+                                        <a href="#" class="bi-bookmark ms-auto me-2">
+                                        </a>
+
+                                        <a href="#" class="bi-heart">
+                                        </a>
+                                    </div>
+
+                                    <div class="d-flex align-items-center">
+                                    </div>
+
+                                    <div class="d-flex align-items-center border-top pt-3">
+
+                                        <a href="job-details.html" class="custom-btn btn ms-auto">Mirar proyectos.</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6 col-12">
+                            <div class="job-thumb job-thumb-box">
+                                <div class="job-image-box-wrap">
+                                    <a href="job-details.html">
+                                        <img src="images/jobs/robo-advisor.jpg" class="job-image img-fluid" alt="">
+                                    </a>
+
+                                    <div class="job-image-box-wrap-info d-flex align-items-center">
+                                    </div>
+                                </div>
+
+                                <div class="job-body">
+                                    <h4 class="job-title">
+                                        <a href="job-details.html" class="job-title-link">Robo-Advisor personalizado.</a>
+                                    </h4>
+
+                                    <div class="d-flex align-items-center">
+                                        <div class="job-image-wrap d-flex align-items-center bg-white shadow-lg mt-2 mb-4">
+                                        </div>
+
+                                        <a href="#" class="bi-bookmark ms-auto me-2">
+                                        </a>
+
+                                        <a href="#" class="bi-heart">
+                                        </a>
+                                    </div>
+
+                                    <div class="d-flex align-items-center">
+                                        
+                                    </div>
+
+                                    <div class="d-flex align-items-center border-top pt-3">
+
+                                        <a href="job-details.html" class="custom-btn btn ms-auto">Mirar Proyectos.</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6 col-12">
+                            <div class="job-thumb job-thumb-box">
+                                <div class="job-image-box-wrap">
+                                    <a href="job-details.html">
+                                        <img src="images/jobs/P2P.jpg" class="job-image img-fluid" alt="">
+                                    </a>
+
+                                    <div class="job-image-box-wrap-info d-flex align-items-center">
+                                    </div>
+                                </div>
+
+                                <div class="job-body">
+                                    <h4 class="job-title">
+                                        <a href="job-details.html" class="job-title-link">Plataforma de préstamos Peer-to-Peer</a>
+                                    </h4>
+                                        
+                                    <div class="d-flex align-items-center">
+                                        <div class="job-image-wrap d-flex align-items-center bg-white shadow-lg mt-2 mb-4">
+                                        </div>
+
+                                        <a href="#" class="bi-bookmark ms-auto me-2">
+                                        </a>
+
+                                        <a href="#" class="bi-heart">
+                                        </a>
+                                    </div>
+
+                                    <div class="d-flex align-items-center">
+                                    </div>
+
+                                    <div class="d-flex align-items-center border-top pt-3">
+
+                                        <a href="job-details.html" class="custom-btn btn ms-auto">Mirar proyectos.</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6 col-12">
+                            <div class="job-thumb job-thumb-box">
+                                <div class="job-image-box-wrap">
+                                    <a href="job-details.html">
+                                        <img src="images/jobs/micro_inversiones.jpg" class="job-image img-fluid" alt="">
+                                    </a>
+
+                                    <div class="job-image-box-wrap-info d-flex align-items-center">
+                                    </div>
+                                </div>
+
+                                <div class="job-body">
+                                    <h4 class="job-title">
+                                        <a href="job-details.html" class="job-title-link">Aplicación de microinversiones.</a>
+                                    </h4>
+
+                                    <div class="d-flex align-items-center">
+                                        <div class="job-image-wrap d-flex align-items-center bg-white shadow-lg mt-2 mb-4">
+                                        </div>
+
+                                        <a href="#" class="bi-bookmark ms-auto me-2">
+                                        </a>
+
+                                        <a href="#" class="bi-heart">
+                                        </a>
+                                    </div>
+
+                                    <div class="d-flex align-items-center">
+                                    </div>
+
+                                    <div class="d-flex align-items-center border-top pt-3">
+
+                                        <a href="job-details.html" class="custom-btn btn ms-auto">Mirar proyectos.</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6 col-12">
+                            <div class="job-thumb job-thumb-box">
+                                <div class="job-image-box-wrap">
+                                    <a href="job-details.html">
+                                        <img src="images/jobs/banca-digital.jpg" class="job-image img-fluid" alt="">
+                                    </a>
+
+                                    <div class="job-image-box-wrap-info d-flex align-items-center">
+                                    </div>
+                                </div>
+
+                                <div class="job-body">
+                                    <h4 class="job-title">
+                                        <a href="job-details.html" class="job-title-link">Banca gigital para no bancarizados.</a>
+                                    </h4>
+                                        
+                                    <div class="d-flex align-items-center">
+                                        <div class="job-image-wrap d-flex align-items-center bg-white shadow-lg mt-2 mb-4"></div>
+                                        </div>
+
+                                        <a href="#" class="bi-bookmark ms-auto me-2">
+                                        </a>
+
+                                        <a href="#" class="bi-heart">
+                                        </a>
+                                    </div>
+
+                                    <div class="d-flex align-items-center">
+                                    </div>
+
+                                    <div class="d-flex align-items-center border-top pt-3">
+
+                                        <a href="job-details.html" class="custom-btn btn ms-auto">Mirar proyectos.</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-12 col-12">
+                            <nav aria-label="Page navigation example">
+                                <ul class="pagination justify-content-center mt-5">
+                                    <li class="page-item">
+                                        <a class="page-link" href="#" aria-label="Previous">
+                                            <span aria-hidden="true">Prev</span>
+                                        </a>
+                                    </li>
+
+                                    <li class="page-item active" aria-current="page">
+                                        <a class="page-link" href="#">1</a>
+                                    </li>
+                                    
+                                    <li class="page-item">
+                                        <a class="page-link" href="#">2</a>
+                                    </li>
+                                    
+                                    <li class="page-item">
+                                        <a class="page-link" href="#">3</a>
+                                    </li>
+
+                                    <li class="page-item">
+                                        <a class="page-link" href="#">4</a>
+                                    </li>
+
+                                    <li class="page-item">
+                                        <a class="page-link" href="#">5</a>
+                                    </li>
+                                    
+                                    <li class="page-item">
+                                        <a class="page-link" href="#" aria-label="Next">
+                                            <span aria-hidden="true">Next</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
+
+           
+        </main>
+
+             <!-- Footer -->
+    <?php
+        include 'footer.php';
+    ?>
+
+        <!-- JAVASCRIPT FILES -->
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/owl.carousel.min.js"></script>
+        <script src="js/counter.js"></script>
+        <script src="js/custom.js"></script>
+
+    </body>
+</html>
