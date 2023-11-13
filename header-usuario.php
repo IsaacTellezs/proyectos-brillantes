83f6c431f1dfd97337497ed07c434f396eefc351
Isaac Tellez <andreescalante34@gmail.com>
2023-11-13 17:15:15 -0600

    imagen en pestaña de navegador

--- header-usuario.php
+++ header-usuario.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Proyectos Brillantes</title>
+    <link rel="icon" href="images/logo simple.svg" type="image/svg">
     </head>
 <body>
     <header id="top">
