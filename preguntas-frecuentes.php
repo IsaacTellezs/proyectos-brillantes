237bac4abcd3f40f614482037f13b1ad7e5c1c5f
MaferMendoza <coloradomendozafernanda@gmail.com>
2023-11-13 16:28:42 -0600

    redirecciones

--- preguntas-frecuentes.php
+++ preguntas-frecuentes.php
@@ -127,7 +127,7 @@
                             <div class="custom-border-btn-wrap d-flex align-items-center mt-lg-4 mt-2">
                                 <a href="#" class="custom-btn custom-border-btn btn me-4">Crea una cuenta</a>
 
-                                <a href="#" class="custom-link">Contacto</a>
+                                <a href="contact.php" class="custom-link">Contacto</a>
                             </div>
                         </div>
 
