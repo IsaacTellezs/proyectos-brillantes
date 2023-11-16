960844095bea18cb9b2c154724bf3c02329dc124
Isaac Tellez <andreescalante34@gmail.com>
2023-11-16 10:36:57 -0600

    Rutas absolutas en headers faltantes

--- header.php
+++ header.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="images/logo simple.svg" type="image/svg">
+    <link rel="icon" href="\crowd\images\logo simple.svg" type="image/svg">
 </head>
 <header id="top">
     <nav class="navbar navbar-expand-lg">
             <div class="container">
-                <a class="navbar-brand d-flex align-items-center" href="index.php">
-                    <img src="./images/Logo simple.svg" class="img-fluid logo-image">
+                <a class="navbar-brand d-flex align-items-center" href="\crowd\index.php">
+                    <img src="\crowd\images\Logo simple.svg" class="img-fluid logo-image">
 
                     <div class="d-flex flex-column">
                         <strong class="logo-text">Proyectos </strong>
@@ -24,36 +24,36 @@
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav align-items-center ms-lg-5">
                         <li class="nav-item">
-                            <a class="nav-link" href="index.php">Pagina Principal</a>
+                            <a class="nav-link" href="\crowd\index.php">Pagina Principal</a>
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link" href="about.php">Quienes Somos</a>
+                            <a class="nav-link" href="\crowd\about.php">Quienes Somos</a>
                         </li>
 
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Categorias</a>
 
                             <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
-                            <li><a class="dropdown-item" href="./categorias/educacion.php">Educación</a></li>
-                            <li><a class="dropdown-item" href="./categorias/NegociosyEmprendimiento.php">Negocios y Emprendimiento</a></li>
-                            <li><a class="dropdown-item" href="./categorias/GobiernoyServicios.php">Gobierno y Servicios Publicos</a></li>
-                            <li><a class="dropdown-item" href="./categorias/SocialySinFines.php">Social y sin fines de Lucro</a></li>
-                            <li><a class="dropdown-item" href="./categorias/Salud.php">Salud</a></li>
+                            <li><a class="dropdown-item" href="\crowd\categorias\educacion.php">Educación</a></li>
+                            <li><a class="dropdown-item" href="\crowd\categorias\NegociosyEmprendimiento.php">Negocios y Emprendimiento</a></li>
+                            <li><a class="dropdown-item" href="\crowd\categorias\GobiernoyServicios.php">Gobierno y Servicios Publicos</a></li>
+                            <li><a class="dropdown-item" href="\crowd\categorias\SocialySinFines.php">Social y sin fines de Lucro</a></li>
+                            <li><a class="dropdown-item" href="\crowd\categorias\Salud.php">Salud</a></li>
                             </ul>
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link" href="contact.php">Contacto</a>
+                            <a class="nav-link" href="\crowd\contact.php">Contacto</a>
                         </li>
 
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Registrate</a>
 
                             <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
-                                <li><a class="dropdown-item" href="registro.php">Registrate como creador</a></li>
+                                <li><a class="dropdown-item" href="\crowd\registro.php">Registrate como creador</a></li>
 
-                                <li><a class="dropdown-item" href="registroInversionista.php">Registrate como inversionista</a></li>
+                                <li><a class="dropdown-item" href="\crowd\registroInversionista.php">Registrate como inversionista</a></li>
                             </ul>
                         </li>
 
@@ -61,9 +61,9 @@
                             <a class="nav-link dropdown-toggle custom-btn btn" href="#">Iniciar Sesion</a>
 
                             <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
-                                <li><a class="dropdown-item" href="login.php">Inicia como creador</a></li>
+                                <li><a class="dropdown-item" href="\crowd\login.php">Inicia como creador</a></li>
 
-                                <li><a class="dropdown-item" href="login-inversionista.php">Inicia como inversionista</a></li>
+                                <li><a class="dropdown-item" href="\crowd\login-inversionista.php">Inicia como inversionista</a></li>
             </div>
         </nav>
     </header>
