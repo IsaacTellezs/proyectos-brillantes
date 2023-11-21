9669f4816460552bf03aa885ef67e2f2aec2b35a
Alberto <cuacho.ax@gmail.com>
2023-11-21 17:26:14 -0600

    Se jalan todos los datos

--- desarrollador/editar_proyecto.php
+++ desarrollador/editar_proyecto.php
@@ -36,57 +36,7 @@ if (isset($_GET['id_proyecto'])) {
     }
 }
 
-// Procesamiento del formulario de edición
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Recuperar los datos del formulario
-    $nuevoProyecto = $_POST['nuevoProyecto'];
-    $nuevaDescripcion = $_POST['nuevaDescripcion'];
-    $categoria = $_POST['categoria'];
-    $meta_financiacion = $_POST['meta_financiacion'];
-    $fecha_inicio = $_POST['fecha_inicio'];
-    $fecha_termino = $_POST['fecha_termino'];
-
-    // Actualizar los datos en la base de datos
-    $queryUpdate = "UPDATE proyectos SET nom_proyecto = '$nuevoProyecto', descripcion = '$nuevaDescripcion', categoria = '$categoria', meta_financiacion = $meta_financiacion, fecha_inicio = '$fecha_inicio', fecha_termino = '$fecha_termino' WHERE id_proyecto = $id_proyecto";
-    $resultUpdate = mysqli_query($conexion, $queryUpdate);
-
-    if ($resultUpdate) {
-        // Éxito: los datos se han actualizado correctamente en la base de datos
-
-        // Verificar si se ha cargado una nueva foto
-        if (isset($_FILES['project_photo']) && $_FILES['project_photo']['error'] === UPLOAD_ERR_OK) {
-            // Directorio de destino para la nueva foto
-            $upload_directory = '../uploads/';
-            // Nombre del archivo
-            $file_name = basename($_FILES['project_photo']['name']);
-            // Ruta completa del archivo en el servidor
-            $target_path = $upload_directory . $file_name;
-
-            // Mover la nueva foto al directorio de carga
-            if (move_uploaded_file($_FILES['project_photo']['tmp_name'], $target_path)) {
-                // La nueva foto se cargó con éxito
-
-                // Actualizar el campo de imagen en la base de datos
-                $queryUpdateImage = "UPDATE proyectos SET imagen = '$file_name' WHERE id_proyecto = $id_proyecto";
-                $resultUpdateImage = mysqli_query($conexion, $queryUpdateImage);
-
-                if (!$resultUpdateImage) {
-                    echo 'Error al actualizar la nueva foto en la base de datos.';
-                }
-            } else {
-                echo "Error al cargar la nueva foto.";
-            }
-        }
-
-        echo 'Datos actualizados correctamente.';
-        // Puedes redirigir al usuario o realizar otras acciones después de la actualización
-    } else {
-        // Error
-        echo 'Error al actualizar los datos.';
-    }
-}
-
-// Cierra la conexión a la base de datos al final, fuera del bloque de procesamiento del formulario
+// Cierra la conexión a la base de datos antes de iniciar el HTML
 mysqli_close($conexion);
 ?>
 
@@ -136,83 +86,82 @@ mysqli_close($conexion);
                 </div>
             </div>
         </header>
-                                
-                                <main>
-    <div class="container">
-        <div class="row d-flex justify-content-center">
-            <div class="col-lg-12 col-md-10 col-12">
-                <form class="custom-form hero-form" method="post" action="editar_proyecto.php?id_proyecto=<?php echo $id_proyecto; ?>" role="form">
-                    <h3 class="text-white mb-3 d-flex justify-content-center">Editar Proyecto</h3>
 
-                    <div class="row">
-                        <div class="col-lg-12 col-12">
-                            <div class="input-group">
-                                <span class="input-group-text" id="basic-addon1"><i class="bi-person custom-icon"></i></span>
-                                <input type="text" name="nuevoProyecto" id="nuevoProyecto" class="form-control" placeholder="Nuevo Nombre del Proyecto" value="<?php echo $Proyecto; ?>" required>
+        <div class="container">
+            <div class="row d-flex justify-content-center">
+                <div class="col-lg-12 col-md-10 col-12">
+                    <form class="custom-form hero-form" method="post" action="editar_proyecto.php?id_proyecto=<?php echo $id_proyecto; ?>" role="form">
+                        <h3 class="text-white mb-3 d-flex justify-content-center">Editar Proyecto</h3>
+
+                        <div class="row">
+                            <div class="col-lg-12 col-12">
+                                <div class="input-group">
+                                    <span class="input-group-text" id="basic-addon1"><i class="bi-person custom-icon"></i></span>
+                                    <input type="text" name="nuevoProyecto" id="nuevoProyecto" class="form-control" placeholder="Nuevo Nombre del Proyecto" value="<?php echo $Proyecto; ?>" required>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-12 col-12">
+                                <div class="input-group">
+                                    <span class="input-group-text" id="basic-addon1"><i class="bi-person custom-icon"></i></span>
+                                    <input type="text" name="nuevaDescripcion" id="nuevaDescripcion" class="form-control" placeholder="Nueva Descripción" value="<?php echo $Descripcion; ?>" required>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-6 col-md-6 col-12">
+                                <div class="input-group">
+                                    <span class="input-group-text" id="basic-addon1"><i class="bi-person custom-icon"></i></span>
+                                    <select class="form-select" id="categoria" name="categoria" required>
+                                        <option value="Educación" <?php echo ($Categoria === 'Educación') ? 'selected' : ''; ?>>Educacion</option>
+                                        <option value="Negocios y emprendimiento" <?php echo ($Categoria === 'Negocios y emprendimiento') ? 'selected' : ''; ?>>Negocios y emprendimiento</option>
+                                        <option value="Gobierno y servicios públicos" <?php echo ($Categoria === 'Gobierno y servicios públicos') ? 'selected' : ''; ?>>Gobierno y servicios publicos</option>
+                                        <option value="Social y sin fines de lucro" <?php echo ($Categoria === 'Social y sin fines de lucro') ? 'selected' : ''; ?>>Social y sin fines de lucro</option>
+                                        <option value="Salud" <?php echo ($Categoria === 'Salud') ? 'selected' : ''; ?>>Salud</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-6 col-md-6 col-12">
+                                <div class="input-group">
+                                    <span class="input-group-text" id="basic-addon1"><i class="bi-person custom-icon"></i></span>
+                                    <input type="number" name="meta_financiacion" id="meta_financiacion" class="form-control" placeholder="Meta de financiacion" value="<?php echo $MetaFinanciacion; ?>" required pattern="[0-9]+" title="Ingrese solo números">
+                                </div>
+                            </div>
+
+                            <!-- Campo fecha_inicio -->
+                            <div class="col-lg-6 col-md-6 col-12">
+                                <div class="input-group">
+                                    <span class="input-group-text" id="basic-addon1"><i class="bi-person custom-icon"></i></span>
+                                    <input type="date" name="fecha_inicio" id="fecha_inicio" class="form-control" placeholder="Fecha de inicio" value="<?php echo $FechaInicio; ?>" required>
+                                </div>
+                            </div>
+
+                            <!-- Campo fecha_termino -->
+                            <div class="col-lg-6 col-md-6 col-12">
+                                <div class="input-group">
+                                    <span class="input-group-text" id="basic-addon1"><i class="bi-person custom-icon"></i></span>
+                                    <input type="date" name="fecha_termino" id="fecha_termino" class="form-control" placeholder="Fecha de término" value="<?php echo $FechaTermino; ?>" required>
+                                </div>
+                            </div>
+
+                            <!-- Campo para cargar la nueva foto -->
+                            <div class="col-lg-12 col-12">
+                                <div class="input-group">
+                                    <label for="project_photo" class="form-label"></label>
+                                    <input type="file" class="form-control" id="project_photo" name="project_photo" accept="image/*">
+                                </div>
+                            </div>
+
+                            <div class="col-lg-12 col-12">
+                                <button type="submit" class="form-control">
+                                    Guardar Cambios
+                                </button>
                             </div>
                         </div>
-
-                        <div class="col-lg-12 col-12">
-                            <div class="input-group">
-                                <span class="input-group-text" id="basic-addon1"><i class="bi-person custom-icon"></i></span>
-                                <input type="text" name="nuevaDescripcion" id="nuevaDescripcion" class="form-control" placeholder="Nueva Descripción" value="<?php echo $Descripcion; ?>" required>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-6 col-md-6 col-12">
-                                    <div class="input-group">
-                                        <span class="input-group-text" id="basic-addon1"><i class="bi-person custom-icon"></i></span>
-                                        <select class="form-select" id="categoria" name="categoria" required>
-                                            <option value="Educación">Educacion</option>
-                                            <option value="Negocios y emprendimiento">Negocios y emprendimiento</option>
-                                            <option value="Gobierno y servicios públicos">Gobierno y servicios publicos</option>
-                                            <option value="Social y sin fines de lucro">Social y sin fines de lucro</option>
-                                            <option value="Salud">Salud</option>
-                                        </select>
-                                    </div>
-                                </div>
-
-                                <div class="col-lg-6 col-md-6 col-12">
-                                    <div class="input-group">
-                                        <span class="input-group-text" id="basic-addon1"><i class="bi-person custom-icon"></i></span>
-                                        <input type="number" name="meta_financiacion" id="meta_financiacion" class="form-control" placeholder="Meta de financiacion" required pattern="[0-9]+" title="Ingrese solo números">
-                                    </div>
-                                </div>
-
-                                <!-- Campo fecha_inicio -->
-                                <div class="col-lg-6 col-md-6 col-12">
-                                    <div class="input-group">
-                                        <span class="input-group-text" id="basic-addon1"><i class="bi-person custom-icon"></i></span>
-                                        <input type="date" name="fecha_inicio" id="fecha_inicio" class="form-control" placeholder="Fecha de inicio" required>
-                                    </div>
-                                </div>
-
-                                <!-- Campo fecha_termino -->
-                                <div class="col-lg-6 col-md-6 col-12">
-                                    <div class="input-group">
-                                        <span class="input-group-text" id="basic-addon1"><i class="bi-person custom-icon"></i></span>
-                                        <input type="date" name="fecha_termino" id="fecha_termino" class="form-control" placeholder="Fecha de término" required>
-                                    </div>
-                                </div>
-
-                                <!-- Campo para cargar la nueva foto -->
-                                <div class="col-lg-12 col-12">
-                                    <div class="input-group">
-                                        <label for="project_photo" class="form-label"></label>
-                                        <input type="file" class="form-control" id="project_photo" name="project_photo" accept="image/*">
-                                    </div>
-                                </div>
-
-                        <div class="col-lg-12 col-12">
-                            <button type="submit" class="form-control">
-                                Guardar Cambios
-                            </button>
-                        </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
 </main>
 
                             </div>
