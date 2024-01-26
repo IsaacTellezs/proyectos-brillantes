fbeb13439005782829f07e061979b4511a6bc0b9
Alberto <cuacho.ax@gmail.com>
2024-01-26 10:21:46 -0600

    mensaje de contacto

--- contacto-guardar.php
+++ contacto-guardar.php
@@ -1,25 +1,22 @@
 <?php
 require 'php/conexion.php';
 
-$nombre_completo  = $_POST['nombre_completo'];
+// Suponiendo que tu conexión ya está establecida en $conectar
 
+$nombre_completo = $_POST['nombre_completo'];
 $motivo = $_POST['motivo'];
+$correo_electronico = $_POST['correo_electronico'];
 
-$correo_electronico= $_POST['correo_electronico'];
+// Utilizar declaraciones preparadas para evitar inyecciones SQL
+$insertar = $conectar->prepare("INSERT INTO contacto (nombre_completo, motivo, correo_electronico) VALUES (?, ?, ?)");
+$insertar->bind_param("sss", $nombre_completo, $motivo, $correo_electronico);
 
-
-$insertar = "INSERT INTO contacto VALUES ('$nombre_completo ','$motivo','$correo_electronico') ";
-
-$query = mysqli_query($conectar, $insertar);
-
-if($query){
-
-echo "<script> alert('correcto');
-location.href = 'confirmacion-contacto.php';
-</script>";
-
-}else{
-echo "<script> alert('incorrecto');
-location.href = 'contact.php';
-</script>";
+if ($insertar->execute()) {
+    echo "<script> alert('correcto'); location.href = 'confirmacion-contacto.php'; </script>";
+} else {
+    echo "<script> alert('incorrecto'); location.href = 'contact.php'; </script>";
 }
+
+$insertar->close();
+$conectar->close();
+?>
--- php/conexion.php
+++ php/conexion.php
@@ -3,7 +3,7 @@
 $host = "127.0.0.1";
 $user = "root";
 $clave = "";
-$bd  = "prueba-crowd-1";
+$bd  = "proyectos-brillantes";
 
 $conectar = mysqli_connect($host,$user,$clave,$bd);
 
